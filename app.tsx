b981b980fc00f6f2af40f2aade3e9edf06797ac5
Hervé <herve@loukoum.io>
2018-09-26 15:53:21 +0200

    delete useless import

--- app.tsx
+++ app.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import FirstComponent from './src/FirstComponent'
 import FilterableContactTable from './src/List'
-import SimpleList from './src/example';
 var data = require('./data.json');
 var data2 = require('./data2.json');
 
